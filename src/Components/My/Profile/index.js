649672923012ec5940f1d839e8553d63cc9db968
Sudip Limbu <responsivesudip@gmail.com>
2019-06-28 14:29:42 +0545

    Add profile display page

--- src/Components/My/Profile/index.js
+++ src/Components/My/Profile/index.js
@@ -1,7 +1,7 @@
 import React, {Fragment} from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import PropTypes from "prop-types";
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import {makeRequest} from "../../../actions/requestAction";
 import {setLastSlot, setLotteryPlayers, setLotterySlot, setLotteryWinners} from "../../../actions/lotteryActions";
@@ -10,6 +10,7 @@ import {setCurrencies, setSettings} from "../../../actions/appStatusAction";
 class Profile extends React.Component {
 
     render() {
+        const {user} = this.props.auth;
         return (
             <Fragment>
                 <ReactCSSTransitionGroup
@@ -22,9 +23,21 @@ class Profile extends React.Component {
                     <div>
                         <div className="row">
                             <div className="col-md-12 mb-md-4">
-                                <div className="card">
+                                <div className="card my-profile">
                                     <div className="card-header">Profile</div>
-                                    <div className="card-body">lorem</div>
+                                    <div className="card-body">
+                                        <img className="card-img-top img-profile" src={user.profile_pic} alt="Profile picture" />
+                                        <ul className="list-group list-group-flush">
+                                            <li className="list-group-item"><strong>Name: </strong> { user.full_name }</li>
+                                            <li className="list-group-item"><strong>Email: </strong> { user.email }</li>
+                                            <li className="list-group-item"><strong>Contact No: </strong> { user.contact_number }</li>
+                                        </ul>
+                                    </div>
+                                    <div className="card-footer">
+                                        <div className="text-center">
+                                            <Link to="/my/played-games">Edit</Link>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
