151f54a17e971314819cd068499b49345962ca83
Sudip Limbu <responsivesudip@gmail.com>
2019-09-11 10:43:16 +0545

    Update max lottery picker numbers to 59

--- src/utils/helper/helperFunctions.js
+++ src/utils/helper/helperFunctions.js
@@ -95,11 +95,12 @@ export function setCookie(name, value, options = {}) {
 
 export function generateRandomLotteryNumber() {
     let lotteryNumber = [];
+    let max = 60;
 
     while (lotteryNumber.length < 6) {
         let randNumber = Math.floor(Math.random() * 100) + 1;
 
-        if (randNumber !== 100 && lotteryNumber.indexOf(randNumber) === -1) {
+        if (randNumber < max && lotteryNumber.indexOf(randNumber) === -1) {
             lotteryNumber.push(randNumber);
         }
     }
