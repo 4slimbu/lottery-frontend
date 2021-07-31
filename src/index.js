fa029b74202cf1c6234304afe54a2b59ceb8effe
Sudip Limbu <4slimbu@gmail.com>
2021-08-01 00:58:00 +0545

    fix for https

--- src/index.js
+++ src/index.js
@@ -1,122 +1,120 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { HashRouter } from 'react-router-dom';
-import './assets/css/bootstrap.css';
-import './assets/css/style.css';
-import AppMain from './Layout/AppMain';
-import configureStore from './config/configureStore';
-import { Provider } from 'react-redux';
+import { HashRouter } from "react-router-dom";
+import "./assets/css/bootstrap.css";
+import "./assets/css/style.css";
+import AppMain from "./Layout/AppMain";
+import configureStore from "./config/configureStore";
+import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthorizationToken from "./utils/axios/setAuthorizationToken";
-import {setAuth, setUser} from "./actions/authActions";
-import Echo from 'laravel-echo';
+import { setAuth, setUser } from "./actions/authActions";
+import Echo from "laravel-echo";
 import {
-    setLotterySlot, addLotterySlotPlayer, setLotteryPlayers,
-    setLotteryData
+  setLotterySlot,
+  addLotterySlotPlayer,
+  setLotteryPlayers,
+  setLotteryData,
 } from "./actions/lotteryActions";
-import {setCurrencies, setSettings} from "./actions/appStatusAction";
-import {getEnv} from "./utils/helper/helperFunctions";
+import { setCurrencies, setSettings } from "./actions/appStatusAction";
+import { getEnv } from "./utils/helper/helperFunctions";
 
-let jwtToken = '';
+let jwtToken = "";
 export const store = configureStore();
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 // check if jwtToken exist in local storage and is valid
 if (localStorage.getItem("jwtToken")) {
-    try {
-        const decodedToken = jwt_decode(localStorage.getItem("jwtToken"));
-        if (decodedToken.exp > (new Date().getTime() / 1000)) {
-            jwtToken = localStorage.getItem("jwtToken");
-            setAuthorizationToken(jwtToken);
-            store.dispatch(setAuth(decodedToken));
-        } else {
-            localStorage.removeItem("jwtToken");
-            setAuthorizationToken(false);
-            store.dispatch(setAuth({}));
-        }
-    } catch (err) {
-        localStorage.removeItem("jwtToken");
-        setAuthorizationToken(false);
-        store.dispatch(setAuth({}));
+  try {
+    const decodedToken = jwt_decode(localStorage.getItem("jwtToken"));
+    if (decodedToken.exp > new Date().getTime() / 1000) {
+      jwtToken = localStorage.getItem("jwtToken");
+      setAuthorizationToken(jwtToken);
+      store.dispatch(setAuth(decodedToken));
+    } else {
+      localStorage.removeItem("jwtToken");
+      setAuthorizationToken(false);
+      store.dispatch(setAuth({}));
     }
+  } catch (err) {
+    localStorage.removeItem("jwtToken");
+    setAuthorizationToken(false);
+    store.dispatch(setAuth({}));
+  }
 }
 
 // set user
 let userId = null;
 if (localStorage.getItem("user")) {
-    try {
-        const user = JSON.parse(localStorage.getItem("user"));
-        userId = user.id;
-        store.dispatch(setUser(user));
-    } catch (err) {
-    }
+  try {
+    const user = JSON.parse(localStorage.getItem("user"));
+    userId = user.id;
+    store.dispatch(setUser(user));
+  } catch (err) {}
 }
 
 // set currencies
 if (localStorage.getItem("currencies")) {
-    try {
-        const currencies = JSON.parse(localStorage.getItem("currencies"));
-        store.dispatch(setCurrencies(currencies));
-    } catch (err) {
-    }
+  try {
+    const currencies = JSON.parse(localStorage.getItem("currencies"));
+    store.dispatch(setCurrencies(currencies));
+  } catch (err) {}
 }
 
 // set settings
 if (localStorage.getItem("settings")) {
-    try {
-        const settings = JSON.parse(localStorage.getItem("settings"));
-        store.dispatch(setSettings(settings));
-    } catch (err) {
-    }
+  try {
+    const settings = JSON.parse(localStorage.getItem("settings"));
+    store.dispatch(setSettings(settings));
+  } catch (err) {}
 }
 
-
-
-const renderApp = Component => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <HashRouter>
-                <Component />
-            </HashRouter>
-        </Provider>,
-        rootElement
-    );
+const renderApp = (Component) => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <HashRouter>
+        <Component />
+      </HashRouter>
+    </Provider>,
+    rootElement
+  );
 };
 
 renderApp(AppMain);
 
-window.io = require('socket.io-client');
+window.io = require("socket.io-client");
 window.Echo = new Echo({
-    broadcaster: 'socket.io',
-    // host: getEnv('APP_SOCKET_URL'),
-    host: window.location.hostname + ':6001',
-    auth: {
-        headers: {
-            'Authorization' : 'Bearer ' + jwtToken
-        }
-    }
+  broadcaster: "socket.io",
+  host: getEnv("APP_SOCKET_URL"),
+  // host: window.location.hostname + ':6001',
+  auth: {
+    headers: {
+      Authorization: "Bearer " + jwtToken,
+    },
+  },
 });
 
-window.Echo.channel('lottery')
-    .listen('LotterySlotCreatedEvent', (e) => {
-        store.dispatch(setLotterySlot(e.data));
-        store.dispatch(setLotteryPlayers({data: []}));
-    }).listen('LotterySlotClosedEvent', (e) => {
-        store.dispatch(setLotteryData(e));
-    }).listen('ParticipantAddedEvent', (e) => {
-        let slot = e.data;
-        let participant = e.participant;
-        store.dispatch(setLotterySlot(slot));
-        store.dispatch(addLotterySlotPlayer(participant));
-    });
+window.Echo.channel("lottery")
+  .listen("LotterySlotCreatedEvent", (e) => {
+    store.dispatch(setLotterySlot(e.data));
+    store.dispatch(setLotteryPlayers({ data: [] }));
+  })
+  .listen("LotterySlotClosedEvent", (e) => {
+    store.dispatch(setLotteryData(e));
+  })
+  .listen("ParticipantAddedEvent", (e) => {
+    let slot = e.data;
+    let participant = e.participant;
+    store.dispatch(setLotterySlot(slot));
+    store.dispatch(addLotterySlotPlayer(participant));
+  });
 
-window.Echo.private('App.User.' + userId)
-    .listen('UserUpdateEvent', (e) => {
-        console.log('UserUpdateEvent', e.token);
-        if (e.token) {
-            localStorage.setItem("jwtToken", e.token);
-            setAuthorizationToken(e.token);
-            this.props.setAuth(jwt_decode(e.token));
-        }
-    });
+window.Echo.private("App.User." + userId).listen("UserUpdateEvent", (e) => {
+  console.log("UserUpdateEvent", e.token);
+  if (e.token) {
+    localStorage.setItem("jwtToken", e.token);
+    setAuthorizationToken(e.token);
+    this.props.setAuth(jwt_decode(e.token));
+  }
+});
