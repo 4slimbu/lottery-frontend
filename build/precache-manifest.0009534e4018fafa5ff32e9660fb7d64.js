f61f9f80e4cbd9aeac2bdee5640498a8ba9a5709
Sudip Limbu <4slimbu@gmail.com>
2021-08-02 19:59:12 +0545

    build update

---
+++ build/precache-manifest.0009534e4018fafa5ff32e9660fb7d64.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8e32cba6d3ec66d676",
+    "url": "/static/js/0.9fcaac5b.chunk.js"
+  },
+  {
+    "revision": "248dd4bb335d43814ddb",
+    "url": "/static/js/1.c956297a.chunk.js"
+  },
+  {
+    "revision": "9d6791be4d49fb2d699a",
+    "url": "/static/css/2.4fffcac4.chunk.css"
+  },
+  {
+    "revision": "9d6791be4d49fb2d699a",
+    "url": "/static/js/2.6fc668ef.chunk.js"
+  },
+  {
+    "revision": "35ad6c272838f4570079",
+    "url": "/static/css/main.902feb7f.chunk.css"
+  },
+  {
+    "revision": "35ad6c272838f4570079",
+    "url": "/static/js/main.25660cdd.chunk.js"
+  },
+  {
+    "revision": "cacfe6d3f88195a1a739",
+    "url": "/static/js/runtime~main.6599b31b.js"
+  },
+  {
+    "revision": "da11fe4acb2832c39fed",
+    "url": "/static/js/5.58569ed2.chunk.js"
+  },
+  {
+    "revision": "5615790fb284c3828eaa",
+    "url": "/static/js/6.763f6f3c.chunk.js"
+  },
+  {
+    "revision": "0ad342cdfcd6f790986c",
+    "url": "/static/js/7.df7d1a33.chunk.js"
+  },
+  {
+    "revision": "2d9bdfac5c5adccc18de",
+    "url": "/static/js/8.692edb2a.chunk.js"
+  },
+  {
+    "revision": "f5deed7f28d35bd73c70",
+    "url": "/static/js/9.d30a7249.chunk.js"
+  },
+  {
+    "revision": "09a9a7258c0cc1555cd0",
+    "url": "/static/js/10.d76cca1e.chunk.js"
+  },
+  {
+    "revision": "99faf7750cedaad67755df2222891a80",
+    "url": "/static/media/new-bg-img.99faf775.jpg"
+  },
+  {
+    "revision": "71f4819eba6f0da9e02da1a758424210",
+    "url": "/index.html"
+  }
+];
